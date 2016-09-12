587557d705d2d0dbb23953c5f25186b66124f895
taintednobodies <taintednobodies@gmail.com>
2016-09-12 13:01:40 -0500

    Added view of each lesson plan

--- lessonplanner/urls.py
+++ lessonplanner/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.lesson_list, name='lesson_list'),
+    url(r'^lesson/(?P<pk>\d+)/$', views.lesson_detail, name='lesson_detail'),
 ]
--- lessonplanner/views.py
+++ lessonplanner/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
 from django.utils import timezone
 from .models import Lesson
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 def lesson_list(request):
     lessons = Lesson.objects.order_by('unit')
     return render(request, 'lessonplanner/lesson_list.html', {'Lessons': lessons })
+
+def lesson_detail(request, pk):
+    lessons = get_object_or_404(Lesson, pk=pk)
+    return render(request, 'lessonplanner/lesson_detail.html', {'Lessons': lessons })
