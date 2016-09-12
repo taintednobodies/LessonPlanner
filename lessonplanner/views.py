1f2e038b5d5eb08477a9567d0bf8217081da8d35
taintednobodies <taintednobodies@gmail.com>
2016-09-12 07:37:17 -0500

    Modified my view.

--- lessonplanner/views.py
+++ lessonplanner/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Lesson
 
 # Create your views here.
 
 def lesson_list(request):
-    return render(request, 'lessonplanner/lesson_list.html', {})
+    lessons = Lesson.objects.order_by('date')
+    return render(request, 'lessonplanner/lesson_list.html', {'Lessons': lessons})
