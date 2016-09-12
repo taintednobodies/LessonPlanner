8d2f39ff7b36aa4dd90ecc8adacf76af3b5cb1d2
taintednobodies <taintednobodies@gmail.com>
2016-09-12 15:03:07 -0500

    Added allowing user to add new posts.

--- lessonplanner/urls.py
+++ lessonplanner/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from . import views
 
+
+
 urlpatterns = [
     url(r'^$', views.lesson_list, name='lesson_list'),
     url(r'^lesson/(?P<pk>\d+)/$', views.lesson_detail, name='lesson_detail'),
+    url(r'^lesson/new/$', views.lesson_new, name='lesson_new'),
 ]
--- lessonplanner/views.py
+++ lessonplanner/views.py
@@ -1,6 +1,8 @@
 from django.utils import timezone
 from .models import Lesson
 from django.shortcuts import render, get_object_or_404
+from .forms import LessonForm
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -9,5 +11,21 @@ def lesson_list(request):
     return render(request, 'lessonplanner/lesson_list.html', {'Lessons': lessons })
 
 def lesson_detail(request, pk):
-    lessons = get_object_or_404(Lesson, pk=pk)
-    return render(request, 'lessonplanner/lesson_detail.html', {'Lessons': lessons })
+    lesson = get_object_or_404(Lesson, pk=pk)
+    return render(request, 'lessonplanner/lesson_detail.html', {'Lesson': lesson })
+'''
+def lesson_new(request):
+    form = LessonForm()
+    return render(request, 'lessonplanner/lesson_edit.html', {'form': form})
+'''
+def lesson_new(request):
+    if request.method == "POST":
+        form = LessonForm(request.POST)
+        if form.is_valid():
+            lesson = form.save(commit=False)
+            lesson.author = request.user
+            lesson.save()
+            return redirect('lesson_detail', pk=lesson.pk)
+    else:
+        form = LessonForm()
+    return render(request, 'lessonplanner/lesson_edit.html', {'form': form})
